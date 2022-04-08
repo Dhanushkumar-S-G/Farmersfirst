a21552874c865631ffea72e6135e2de1c58d8af2
Dhanushkumar-S-G <dhanushkumarganapathy@gmail.com>
2022-04-08 20:51:48 +0530

    first prototype

--- Agrisales/settings.py
+++ Agrisales/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6))%7+h)vu3ulbd-z*_$k=*%9p=+2mg86gcq04t(d4%(5bz!!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://farmersfirst.herokuapp.com/']
 
 
 # Application definition
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
